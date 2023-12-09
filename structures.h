8eac5313c546fadf8f2ef0f173f776a8fba1e466
Sunghyun Ko <sunko@c3r3s3.42seoul.kr>
2023-12-09 23:56:25 +0900

    Add structures.h file

---
+++ structures.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structures.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunko <sunko@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 23:37:44 by sunko             #+#    #+#             */
+/*   Updated: 2023/12/09 23:37:55 by sunko            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURES_H
+# define STRUCTURES_H
+
+typedef struct s_vec3	t_vec3;
+typedef struct s_vec3	t_point3;
+typedef struct s_vec3	t_color3;
+
+struct s_vec3
+{
+	double	x;
+	double	y;
+	double	z;
+};
+
+#endif
