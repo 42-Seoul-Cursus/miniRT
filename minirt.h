3321b31c25123a9d2eb3de68de4886656ae97412
Sunghyun Ko <sunko@cx2r8s4.42seoul.kr>
2023-12-11 00:02:49 +0900

    Add minirt.h file

---
+++ minirt.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunko <sunko@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 23:27:59 by sunko             #+#    #+#             */
+/*   Updated: 2023/12/10 23:40:59 by sunko            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "structures.h"
+
+void	init_mlx(t_mlx_data *mlx_data);
+void	execute_mlx(t_mlx_data *mlx_data);
+
+#endif
