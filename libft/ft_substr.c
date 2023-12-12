cfa5c82560be4543804edd9a9bc65b86ee69cbb7
seungwonme <seungwonan.me@gmail.com>
2023-12-12 14:20:40 +0900

    Modified ft_substr in libft

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -12,26 +12,26 @@
 
 #include "libft.h"
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
-	size_t	len;
+	size_t	s_len;
 
-	len = ft_strlen(s);
-	if (len < start)
+	s_len = ft_strlen(s);
+	if (s_len < start)
 	{
 		str = ft_calloc(1, 1);
 		return (str);
 	}
-	else if (len > len - start)
-		str = ft_calloc(len - start + 1, sizeof(char));
+	else if (len > s_len - start)
+		str = ft_calloc( s_len - start + 1, sizeof(char));
 	else
 		str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (0);
 	i = 0;
-	while (s[start] != '\0' && i < len && start < len)
+	while (s[start] != '\0' && i < len && start < s_len)
 	{
 		str[i] = s[start];
 		++i;
