fc28f52b17c2beddd14bda5a5532b817323f450a
Seungwon An <seunan@cx1r2s3.42seoul.kr>
2023-12-13 12:52:35 +0900

    Create sphere

--- include/ray.h
+++ include/ray.h
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ray.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunan <seunan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/09 23:23:22 by sunko             #+#    #+#             */
-/*   Updated: 2023/12/13 12:34:42 by seunan           ###   ########.fr       */
+/*   Created: 2023/12/13 00:18:07 by sunko             #+#    #+#             */
+/*   Updated: 2023/12/13 12:36:06 by seunan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt.h"
-#include "parse.h"
-#include "vector.h"
+#ifndef RAY_H
+# define RAY_H
 
-int	main(int argc, char *argv[])
-{
-	t_mlx_data	mlx_data;
-	t_vars		vars;
+# include "structures.h"
 
-	is_valid_arg(argc, argv);
-	init_vars(&vars);
-	parse_rt(&vars, argv[1]);
-	is_valid_rt(&vars);
-	init_mlx(&mlx_data);
-	world2view(&vars);
-	render(&vars, &mlx_data);
-	execute_mlx(&mlx_data);
-	return (0);
-}
+t_ray		ray(t_point3 orig, t_vec3 dir);
+t_point3	ray_at(t_ray *ray, const double t);
+
+#endif
--- src/color.c
+++ src/color.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunan <seunan@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 12:48:22 by seunan            #+#    #+#             */
+/*   Updated: 2023/12/13 12:48:50 by seunan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "structures.h"
 
-t_color3	 get_color_int_to_real(t_color3 rgb)
+t_color3	get_color_int_to_real(t_color3 rgb)
 {
 	t_color3	out;
 
@@ -14,8 +26,8 @@ t_color3	get_color_real_to_int(t_color3 rgb)
 {
 	t_color3	out;
 
-	out.x = rgb.x * 255;
-	out.y = rgb.y * 255;
-	out.z = rgb.z * 255;
+	out.x = rgb.x * 255.999;
+	out.y = rgb.y * 255.999;
+	out.z = rgb.z * 255.999;
 	return (out);
 }
