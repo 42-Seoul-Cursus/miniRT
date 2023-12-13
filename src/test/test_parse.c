59cbf5c874339664a71048a91cc407ca28cd5daa
Seungwon An <seunan@cx1r2s3.42seoul.kr>
2023-12-13 17:21:13 +0900

    Parsing light variables into list

--- src/test/test_parse.c
+++ src/test/test_parse.c
@@ -2,6 +2,12 @@
 
 void	test_parse(t_vars *vars)
 {
+	t_list		*tmp;
+	t_light		*light;
+	t_plane		*plane;
+	t_sphere	*sphere;
+	t_cylinder	*cylinder;
+
 	// ambient
 	printf("A %.1f %.1f,%.1f,%.1f\n", vars->ambient.lighting_ratio, vars->ambient.rgb.x, vars->ambient.rgb.y, vars->ambient.rgb.z);
 	printf("\n");
@@ -10,15 +16,16 @@ void	test_parse(t_vars *vars)
 	printf("C %.1f,%.1f,%.1f %.1f,%.1f,%.1f %.1f\n", vars->camera.view_point.x, vars->camera.view_point.y, vars->camera.view_point.z, vars->camera.direct_v.x, vars->camera.direct_v.y, vars->camera.direct_v.z, vars->camera.fov);
 
 	// light
-	printf("L %.1f,%.1f,%.1f %.1f %.1f,%.1f,%.1f\n", vars->light.light_point.x, vars->light.light_point.y, vars->light.light_point.z, vars->light.brightness_ratio, vars->light.rgb.x, vars->light.rgb.y, vars->light.rgb.z);
+	tmp = vars->light;
+	while (tmp != NULL)
+	{
+		light = tmp->content;
+		printf("L %.1f,%.1f,%.1f %.1f %.1f,%.1f,%.1f\n", light->light_point.x, light->light_point.y, light->light_point.z, light->brightness_ratio, light->rgb.x, light->rgb.y, light->rgb.z);
+		tmp = tmp->next;
+	}
 	printf("\n");
 
 	// objects Verify identifiers
-	t_plane		*plane;
-	t_sphere	*sphere;
-	t_cylinder	*cylinder;
-	t_list		*tmp;
-
 	tmp = vars->objects;
 	while (tmp != NULL)
 	{
